5dde3ed427353fa0c7bee1c60da6bbbcfcda5da3
Monal Valia <monalvalia@Monals-Mac-mini.local>
2026-02-19 19:32:42 -0600

    Fix Vercel deployment with dynamic base path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/gh300-practice-exam/',
+  base: process.env.VERCEL ? '/' : '/gh300-practice-exam/',
 })
